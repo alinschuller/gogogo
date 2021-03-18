12e50cdab03ae88da7edaf180d85826ecc35f97e
Alin-Mihai Schüller <schulleralinmihai@gmail.com>
2021-03-18 13:37:02 +0200

    Add language support for relay work checking

--- hello.go
+++ hello.go
@@ -2,15 +2,36 @@ package main
 
 import "fmt"
 
+const romanian = "Romanian"
+const german = "German"
+const french = "French"
 const englishRelayWorkPrefix = "Is the relay ready, "
+const romanianRelayWorkPrefix = "E gata releul, "
+const frenchRelayWorkPrefix = "Le relais est-il prêt, "
+const germanRelayWorkPrefix = "Ist das Relais bereit, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "freaks"
 	}
-	return englishRelayWorkPrefix + name + "?"
+
+	return relayWorkPrefix(language) + name + "?"
+}
+
+func relayWorkPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchRelayWorkPrefix
+	case romanian:
+		prefix = romanianRelayWorkPrefix
+	case german:
+		prefix = germanRelayWorkPrefix
+	default:
+		prefix = englishRelayWorkPrefix
+	}
+	return 
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -12,16 +12,34 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("checking relay work with people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Is the relay ready, Chris?"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("check relay work with freaks when empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Is the relay ready, freaks?"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Romanian", func(t *testing.T) {
+		got := Hello("Mihai", "Romanian")
+		want := "E gata releul, Mihai?"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Mishel", "French")
+		want := "Le relais est-il prêt, Mishel?"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in German", func(t *testing.T) {
+		got := Hello("Michael", "German")
+		want := "Ist das Relais bereit, Michael?"
 		assertCorrectMessage(t, got, want)
 	})
 
