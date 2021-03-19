6199d8c11504cb96b2d8752502cdd9318b98ab5d
Alin-Mihai Schüller <schulleralinmihai@gmail.com>
2021-03-19 09:32:23 +0200

    Add extra test case to overwrite value

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -3,7 +3,10 @@ package main
 import "errors"
 
 type Dictionary map[string]string
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+	ErrNotFound = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -15,8 +18,9 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
+func (d Dictionary) Add(word, definition string) error {
 	d[word] = definition
+	return nil
 }
 
 func Search(dictionary map[string]string, word string) string {
