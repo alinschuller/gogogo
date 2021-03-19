9d1c8527adce750906fa76d772a568c6458045c0
Alin-Mihai Schüller <schulleralinmihai@gmail.com>
2021-03-19 10:50:00 +0200

    Add Error Handling for Update

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,6 +4,7 @@ type Dictionary map[string]string
 const (
 	ErrNotFound = DictionaryErr("could not find the word you were looking for")
 	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 type DictionaryErr string
@@ -36,8 +37,19 @@ func (d Dictionary) Add(word, definition string) error {
 	return nil
 }
 
-func (d Dictionary) Update(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
 }
 
 func Search(dictionary map[string]string, word string) string {
